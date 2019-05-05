688388f585265bbf55000ff6154a1aee30d298a1
leaderg <leader.g@gmail.com>
2019-05-05 21:10:02 +0000

    Testing https with simple request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,18 @@
 //require https module
 var https = require('https');
-console.log("Success");
+
+//create test path for https request.
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+//called by https when the request is made.
+var callback = () => {
+  console.log('Response: Handler callback!');
+}
+
+//showing asynchronous behaviour:
+console.log("I'm about to make the request");
+https.request(options, callback).end();
+console.log("I've made the request!");
